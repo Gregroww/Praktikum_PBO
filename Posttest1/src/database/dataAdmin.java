65d5209d31171c2adefd72cb0b4be5c23568f7a7
Rafif Zahran Haryadi <rafif.zahran.haryadi@gmail.com>
2025-03-17 23:43:32 +0800

    commit to main

---
+++ Posttest1/src/database/dataAdmin.java
@@ -0,0 +1,9 @@
+package database;
+import entity.Admin;
+
+public class dataAdmin {
+    public Admin[] dataAdmin = new Admin[10];
+    public dataAdmin(){
+        dataAdmin[0] = new Admin("admin", "admin");
+    }
+}
